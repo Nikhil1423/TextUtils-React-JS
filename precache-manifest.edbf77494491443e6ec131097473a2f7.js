aab65da20fa5252273494be37d6cfa3727ef40a9
Nikhil <nikhilmah23@gmail.com>
2022-04-08 19:46:33 +0530

    Updates

---
+++ precache-manifest.edbf77494491443e6ec131097473a2f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b2a073d902a484c3b163430e8b6ca7",
+    "url": "/TextUtils-React-JS/index.html"
+  },
+  {
+    "revision": "5d81db1406108cead360",
+    "url": "/TextUtils-React-JS/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "2698b6766b0ee112c37e",
+    "url": "/TextUtils-React-JS/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5d81db1406108cead360",
+    "url": "/TextUtils-React-JS/static/js/2.a542fa94.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/TextUtils-React-JS/static/js/2.a542fa94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2698b6766b0ee112c37e",
+    "url": "/TextUtils-React-JS/static/js/main.b74704c6.chunk.js"
+  },
+  {
+    "revision": "84438a2dbd0d68c8ab88",
+    "url": "/TextUtils-React-JS/static/js/runtime-main.9803b485.js"
+  }
+]);
